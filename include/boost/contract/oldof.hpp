ddff0730d0449831223802b5bb274e43102453fe
Lorenzo Caminiti <lorcaminiti@gmail.com>
2015-01-28 12:40:34 -0800

    pasring postconditions

---
+++ include/boost/contract/oldof.hpp
@@ -0,0 +1,10 @@
+
+#ifndef BOOST_CONTRACT_OLDOF_HPP_
+#define BOOST_CONTRACT_OLDOF_HPP_
+
+#include <boost/contract/ext_/preprocessor/traits/oldof.hpp>
+
+#define BOOST_CONTRACT_OLDOF BOOST_CONTRACT_EXT_PP_TRAITS_OLDOF_OPERATOR
+
+#endif // #include guard
+
