22bc450de092b637fb8019352ec9480eadca8783
Lorenzo Caminiti <lorcaminiti@gmail.com>
2019-01-11 15:37:52 -0800

    fixing markups of some expected failures in the test regression table

--- test/specify/auto_error.cpp
+++ test/specify/auto_error.cpp
@@ -7,7 +7,7 @@
 // Test auto error (for free func, but same for all contracts).
 
 #if defined(__cplusplus) && __cplusplus >= 201703L // On C++17 or later.
-    #error "C++17 guaranteed copy elision invalidates this test so forcing test's expected complier error"
+    #error "C++17 copy elision invalidates test so forcing expected failure"
 #else
 
 #include <boost/contract/function.hpp>
